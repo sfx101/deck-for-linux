8792eb2c75665bba223f91986b222eaa9fc365e7
Subhadip N <subhadip@get-deck.com>
2021-10-22 17:12:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 #Set up the required package
-echo "**********************************************************************************";
-echo "Update the apt package index and install packages to allow apt to use a repository over HTTPS:"
-echo "**********************************************************************************";
+echo "Running apt update, installing dependencies"
 sudo apt update
 pkgs='curl uidmap apt-transport-https ca-certificates gnupg lsb-release'
 if ! dpkg -s $pkgs >/dev/null 2>&1; then
@@ -15,20 +13,13 @@ if ! dpkg -s $pkgs >/dev/null 2>&1; then
 fi
 
 #If you installed Docker 20.10 or later without RPM/DEB packages, you should have
-echo "**********************************************************************************";
-echo "If you installed Docker 20.10 or later without RPM/DEB packages, you should have ";
-echo "**********************************************************************************";
+echo "Configuring docker rootless access";
 curl -fsSL https://get.docker.com/rootless | sh
-export PATH=/home/test/bin:$PATH
+export PATH=/home/$USER/bin:$PATH
 export DOCKER_HOST=unix:///run/user/$USER/docker.sock
 systemctl --user enable docker && sudo loginctl enable-linger $(whoami)
-export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
-
 
 #Docker gpg add
-echo "**********************************************************************************";
-echo "Docker gpg add";
-echo "**********************************************************************************";
 FILE=/usr/share/keyrings/docker-archive-keyring.gpg
 if [ -f "$FILE" ]; then 
 	echo "$FILE exists"
@@ -36,17 +27,10 @@ else
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 fi
 #Set up the stable repository.
-echo "**********************************************************************************";
-echo "Set up the stable repository.";
-echo "**********************************************************************************";
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
- #Install Docker Engine
-echo "**********************************************************************************";
-echo "Install Docker Engine";
-echo "**********************************************************************************";
-sudo apt-get update
+#Install Docker Engine
 pkgs='docker-ce docker-ce-cli containerd.io'
 if ! dpkg -s $pkgs >/dev/null 2>&1; then
   sudo apt-get install -y $pkgs
@@ -54,46 +38,19 @@ fi
 
 
 #List the versions available in your repo
-echo "**********************************************************************************";
-echo "List the versions available in your repo";
-echo "**********************************************************************************";
 apt-cache madison docker-ce
-# Group Add
-echo "**********************************************************************************";
-echo "Docker group Add"
-echo "**********************************************************************************";
 sudo usermod -aG docker $USER
 
-echo "**********************************************************************************";
-echo "Service start with user mode";
-echo "**********************************************************************************";
+echo "Staring docker";
 systemctl --user start docker
 systemctl --user enable docker
 sudo loginctl enable-linger $(whoami)
-#Checking You need to specify either the socket path or the CLI context explicitly.
-echo "**********************************************************************************";
-echo "Checking You need to specify either the socket path or the CLI context explicitly.";
-echo "**********************************************************************************";
-##Check and install docker compose####
-echo "**********************************************************************************";
-echo "##Check and install docker compose####";
-echo "**********************************************************************************";
+#Install docker-compose, neofetch
 pkgs='docker-compose neofetch'
 if ! dpkg -s $pkgs >/dev/null 2>&1; then
   sudo apt-get install -y $pkgs
 fi
 
-############## IF not started docker please run below command ################
-echo "**********************************************************************************";
-echo "IF not started docker please run below command";
-echo "**********************************************************************************";
-echo "sudo usermod -aG docker $USER";
-#sudo usermod -aG docker $USER
-echo "systemctl --user start docker";
-#systemctl --user start docker
-echo "systemctl --user enable docker";
-#systemctl --user enable docker
-echo "If https not working"
 sudo setcap 'cap_net_bind_service=+eip' /opt/DECK/deck
 sudo sh -c "echo '/opt/DECK/' >> /etc/ld.so.conf.d/deck.conf"
 sudo ldconfig
@@ -101,9 +58,7 @@ echo "sudo chmod 664 /var/run/docker.sock";
 systemctl start docker
 sudo chmod 666 /var/run/docker.sock
 
-############### End of install#######################
+
 clear
 neofetch
-echo "**********************************************************************************";
-echo "Thanking you select Deck + Docker Application";
-echo "**********************************************************************************";
+echo "All set and done.";
